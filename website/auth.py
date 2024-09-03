8b7a6c8a2d1e59cb74fe99b1cffc42593834deec
zenbladee <seloreshernandez@gmail.com>
2024-09-03 19:57:54 +0000

    commit

--- website/auth.py
+++ website/auth.py
@@ -20,24 +20,25 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         user = User.query.filter_by(email=email).first() #New thing added
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match', category='error')
         elif len(password1) < 7:
             flash('Password must be greater than 6 characters', category='error')
         else:
-            new_user = User(email=email, firstName= firstName, password=generate_password_hash(password1, method='sha256'))
+            new_user = User(email=email, first_name= first_name, password=generate_password_hash(
+                password1, method='scrypt'))
             db.session.add(new_user)
             db.session.commit()
-            flash('Account Created!', category='success')
+            flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
