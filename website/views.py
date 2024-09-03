5b262888dfc4224ad54fa69e810a1c1dc0b7876a
zenbladee <seloreshernandez@gmail.com>
2024-09-03 23:03:27 +0000

    commit

--- website/views.py
+++ website/views.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template #Writing standard routes
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__) #URLs are defined here #Blueprint is defined
 
 @views.route('/') #we can type in slash to navigate the website
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
 
 
 
